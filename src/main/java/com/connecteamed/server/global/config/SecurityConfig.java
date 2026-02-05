20700f1b3e09b4b4f117a343744f8f8cfb29bf3e
fbehdgus <danny90697@gmail.com>
2026-02-05 16:33:10 +0900

    feat: cors 허용 경로 추가

--- src/main/java/com/connecteamed/server/global/config/SecurityConfig.java
+++ src/main/java/com/connecteamed/server/global/config/SecurityConfig.java
@@ -115,7 +115,8 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "https://api.connecteamed.shop",
-                "https://connecteamed.shop"
+                "https://connecteamed.shop",
+                "https://connecteamed.vercel.app"
 
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
